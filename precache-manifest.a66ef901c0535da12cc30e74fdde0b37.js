9edfbd5431efb4b9f8d679e225047f700bc0ea76
tsetie <tsetie@yahoo.com>
2023-09-04 23:09:53 -0500

    Updates

---
+++ precache-manifest.a66ef901c0535da12cc30e74fdde0b37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d59087bb06700e19412",
+    "url": "/Weather-App/static/css/main.c3895219.chunk.css"
+  },
+  {
+    "revision": "2d59087bb06700e19412",
+    "url": "/Weather-App/static/js/main.f8241092.chunk.js"
+  },
+  {
+    "revision": "89bb695666ed5c38f231",
+    "url": "/Weather-App/static/js/runtime~main.671df5a1.js"
+  },
+  {
+    "revision": "1f224d939d00efddb060",
+    "url": "/Weather-App/static/js/2.b565c5bf.chunk.js"
+  },
+  {
+    "revision": "389eb4d65627565da7992c6e4a2010b4",
+    "url": "/Weather-App/index.html"
+  }
+];
